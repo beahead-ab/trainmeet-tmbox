7a16557730a829241b3a0aca254ba5286ba45f25
trainmeet-version <version@trainmeet.app>
2026-09-20 07:51:39 +0000

    Version 0.6.0 [skip version]

--- firmware/esp32/TrainMeetTMBox.ino
+++ firmware/esp32/TrainMeetTMBox.ino
@@ -34,7 +34,7 @@
 #include "meet_scope.h"
 #include "language_menu.h"
 
-constexpr char FIRMWARE_VERSION[] = "0.5.0";
+constexpr char FIRMWARE_VERSION[] = "0.6.0";
 constexpr char DISCOVERY_SERVICE[] = "tmbox";
 constexpr uint16_t DEFAULT_MQTT_PORT = 1883;
 constexpr unsigned long SAVED_WIFI_WINDOW_MS = 15000;
